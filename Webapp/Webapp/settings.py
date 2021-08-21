503b4912b9834e28730bc2c5fc5965af436a4304
yahya benzha <yahya.benzha1@gmail.com>
2021-08-21 11:57:13 +0100

    Update settings.py

--- Webapp/Webapp/settings.py
+++ Webapp/Webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bxc99r6ff$(0$+$3w!u9!k3k!$f$8u&dn7o^_nx#46f!lbr9m='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
